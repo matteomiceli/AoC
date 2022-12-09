b4ad80923a06bcdc440fed8a9f1936475957a481
matteomiceli <omatteomiceli@gmail.com>
2022-12-08 22:33:08 -0800

    day 9 complete

--- day9/nine.js
+++ day9/nine.js
@@ -2,10 +2,22 @@ const { getData } = require("../helpers/helpers");
 const movements = getData("./data.txt").array.map((move) => move.split(" "));
 
 const tVisited = new Set();
+const rope = {
+  0: { x: 0, y: 0 },
+  1: { x: 0, y: 0 },
+  2: { x: 0, y: 0 },
+  3: { x: 0, y: 0 },
+  4: { x: 0, y: 0 },
+  5: { x: 0, y: 0 },
+  6: { x: 0, y: 0 },
+  7: { x: 0, y: 0 },
+  8: { x: 0, y: 0 },
+  9: { x: 0, y: 0 },
+};
 
 // part 1
-const tail = { x: 0, y: 0 };
-const head = { x: 0, y: 0 };
+// const tail = { x: 0, y: 0 };
+// const head = { x: 0, y: 0 };
 
 function movement(vector, dir) {
   switch (dir) {
@@ -46,16 +58,16 @@ function tailAdjustment(vHead, vTail) {
 
   // horizontal adjust
   if (adjustX > 0) {
-    movement(tail, "R");
+    movement(vTail, "R");
   } else if (adjustX < 0) {
-    movement(tail, "L");
+    movement(vTail, "L");
   }
 
   // vertical adjust
   if (adjustY > 0) {
-    movement(tail, "U");
+    movement(vTail, "U");
   } else if (adjustY < 0) {
-    movement(tail, "D");
+    movement(vTail, "D");
   }
 }
 
@@ -64,11 +76,18 @@ movements.forEach((move, i) => {
   const length = move[1];
 
   for (let i = 0; i < length; i++) {
-    movement(head, dir);
-    if (areApart(head, tail)) {
-      tailAdjustment(head, tail);
+    movement(rope[0], dir);
+    for (let j = 0; j < Object.values(rope).length; j++) {
+      if (j === Object.values(rope).length - 1) break;
+
+      const segment = rope[j];
+      const trailing = rope[j + 1];
+
+      if (areApart(segment, trailing)) {
+        tailAdjustment(segment, trailing);
+      }
+      tVisited.add(JSON.stringify(rope[9]));
     }
-    tVisited.add(JSON.stringify(tail));
   }
 });
 
